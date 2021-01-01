5d9dacbcdcd97d898455c93e42f07376c0441867
Nabendu <nabendu.biswas@gmail.com>
2021-01-01 19:52:36 +0530

    Webpack react complete

---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require("path");
+
+module.exports = {
+    mode: "development",
+    entry: "./index.js",
+    output: {
+        path: path.resolve(__dirname, "public"),
+        filename: "main.js"
+    },
+    target: "node",
+    devServer: {
+        port: "9500",
+        contentBase: ["./public"],
+        open: true
+    },
+    resolve: {
+        extensions: [".js",".jsx",".json"]
+    },
+    module: {
+        rules: [
+            {
+                test: /\.(js|jsx)$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            }
+        ]
+    }
+}
